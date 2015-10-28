a7052f5b65f91b77caa31125ac591af9cf17e952
Weckar <mail@weckarlive.com>
2015-10-28 22:59:32 +0100

    Code Cleanup

--- main/java/com/weckar/modExperiment/worldgen/WorldGenerator.java
+++ main/java/com/weckar/modExperiment/worldgen/WorldGenerator.java
@@ -8,6 +8,7 @@ import com.weckar.modExperiment.init.ModFluids;
 import com.weckar.modExperiment.utility.LogHelper;
 
 import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
@@ -26,52 +27,49 @@ public class WorldGenerator implements IWorldGenerator
 
 	private void generateMournland(World world, Random random, int chunkX, int chunkZ) 
 	{
+		if(random.nextInt(100)==0){//TODO make config option.
+			generateGodsBloodWell(world,random,chunkX,chunkZ);
+		}
+	}
+
+
+	private void generateGodsBloodWell(World world, Random random, int chunkX, int chunkZ){
 		//Generate GodsBlood
-		if(random.nextInt(40)==0){//TODO make config option.
-			//BASIN
-			int r = random.nextInt(10)+2;
-			int y = random.nextInt(20)+5+r;
-			int x = chunkX*16 + random.nextInt(16);
-			int z = chunkZ*16 + random.nextInt(16);
-			for(int ix = x-r;ix<=x+r;ix++)
-			{
-				for(int iy = y-r;iy<=y+r;iy++)
-				{
-					for(int iz = z-r;iz<=z+r;iz++)
-					{
-						//Make it a sphere
-						double trueR = Math.sqrt(Math.pow((x-ix),2)+Math.pow((y-iy),2)+Math.pow((z-iz),2));
-						if (trueR<r){
-							world.setBlock(ix,iy,iz,ModFluids.godsBlood);
-						}
+		int r = random.nextInt(10)+2;
+		int y = random.nextInt(20)+5+r;
+		int x = chunkX*16 + random.nextInt(16);
+		int z = chunkZ*16 + random.nextInt(16);
+		//BASIN
+		for(int ix = x-r;ix<=x+r;ix++){
+			for(int iy = y-r;iy<=y+r;iy++){
+				for(int iz = z-r;iz<=z+r;iz++){
+					//Make it a sphere
+					double trueR = Math.sqrt(Math.pow((x-ix),2)+Math.pow((y-iy),2)+Math.pow((z-iz),2));
+					if (trueR<r){
+						world.setBlock(ix,iy,iz,ModFluids.godsBlood);
 					}
 				}
 			}
-			//PILLAR
-			for(int iy = y;!world.isAirBlock(x,iy,z) || iy<64 ;iy++)
-			{
-				world.setBlock(x,iy,z,ModFluids.godsBlood);
-				//TOP
-				if(world.isAirBlock(x,iy+2,z)&&iy+2>=64){
-					world.setBlock(x-1,iy,z-1,ModFluids.godsBlood);
-					world.setBlock(x-1,iy,z,ModFluids.godsBlood);
-					world.setBlock(x-1,iy,z+1,ModFluids.godsBlood);
-					world.setBlock(x,iy,z-1,ModFluids.godsBlood);
-					world.setBlock(x,iy,z+1,ModFluids.godsBlood);
-					world.setBlock(x+1,iy,z-1,ModFluids.godsBlood);
-					world.setBlock(x+1,iy,z,ModFluids.godsBlood);
-					world.setBlock(x+1,iy,z+1,ModFluids.godsBlood);
+		}
+		//PILLAR
+		for(int iy = y; y<128 ;iy++)
+		{
+			world.setBlock(x,iy,z,ModFluids.godsBlood);
+			//TOP
+			if(world.isAirBlock(x,iy+2,z)&&iy+2>=64){
+				setBlocksInArea(world,x-1,iy,z-1,x+1,iy,z+1,ModFluids.godsBlood);
+				setBlocksInArea(world,x-1,iy+1,z-1,x+1,iy+1,z+1,Blocks.air);
+				world.setBlock(x,iy+1,z,ModFluids.godsBlood);
+				break;
+			}
+		}
+	}
 
-					world.setBlockToAir(x-1,iy+1,z-1);
-					world.setBlockToAir(x-1,iy+1,z);
-					world.setBlockToAir(x-1,iy+1,z+1);
-					world.setBlockToAir(x,iy+1,z-1);
-					world.setBlock(x,iy+1,z,ModFluids.godsBlood);
-					world.setBlockToAir(x,iy+1,z+1);
-					world.setBlockToAir(x+1,iy+1,z-1);
-					world.setBlockToAir(x+1,iy+1,z);
-					world.setBlockToAir(x+1,iy+1,z+1);
-					break;
+	private void setBlocksInArea(World world, int x1, int y1, int z1, int x2, int y2, int z2, Block block){
+		for(int x=Math.min(x1,x2);x<=Math.max(x1,x2);x++){
+			for(int y=Math.min(y1,y2);y<=Math.max(y1,y2);y++){
+				for(int z=Math.min(z1,z2);z<=Math.max(z1,z2);z++){
+					world.setBlock(x,y,z,block);
 				}
 			}
 		}
